e33fffa4b6c8da079ea934dd4d3ea839f2f64dbc
Andy Garcia <barcag07@icloud.com>
2024-05-26 18:52:02 -0500

    Change div color when hovered over
    Each gridItem div was given an eventListener that changes the
    background color when it is hovered over.

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 const gridContainer = document.querySelector("div");
 
 const grid = document.createElement("div");
-grid.setAttribute("style", "display: flex; flex-wrap: wrap; width: 400px; height: 400px; outline: 3px solid red;");
+grid.setAttribute("style", "display: flex; flex-wrap: wrap; width: 400px; height: 400px; outline: 3px solid black;");
 
 // const test = document.createElement("p");
 // gridContainer.appendChild(test);
@@ -11,9 +11,14 @@ grid.setAttribute("style", "display: flex; flex-wrap: wrap; width: 400px; height
 for (let i = 1; i <= 256; i++) {
      const gridItem = document.createElement("div");
      gridItem.classList.add("grid-item","item-" + i);
-     // gridItem.textContent = i;
      gridItem.setAttribute ("style", "height: 25px; width: 25px; outline: 1px solid blue;");
+
+     //Adding hover event listener to each grid item
+     gridItem.addEventListener("mouseover", function() {
+          gridItem.style.backgroundColor = "red";
+     });
      grid.appendChild(gridItem);
 }
+
 
 gridContainer.appendChild(grid);
