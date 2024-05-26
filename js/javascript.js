f3758cea9c3cff12202154d3da7e2c928aac1e0f
Andy Garcia <barcag07@icloud.com>
2024-05-26 17:39:05 -0500

    Create a 16 x 16 grid
    256 (16 x 16) grid items were created with a for loop and added as
    children to the grid container. Attributes were set to the grid
    container and grid items so that they can fit in rows of 16.

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,15 @@
 const gridContainer = document.querySelector("div");
+gridContainer.setAttribute("style", "display: flex; flex-wrap: wrap; width: 400px; height: 400px; outline: 3px solid red;");
 
 // const test = document.createElement("p");
 // gridContainer.appendChild(test);
 
-//Create the 16 grids that go into the grid container
-for (let i = 0; i < 16; i++) {
-     const grid = document.createElement("div");
-     gridContainer.append(grid);
+//Create the 16 x 16 (256) grids that go into the grid container
+
+for (let i = 1; i <= 256; i++) {
+     const gridItem = document.createElement("div");
+     gridItem.classList.add("grid-item","item-" + i);
+     // gridItem.textContent = i;
+     gridItem.setAttribute ("style", "height: 25px; width: 25px; outline: 1px solid blue;");
+     gridContainer.appendChild(gridItem);
 }
