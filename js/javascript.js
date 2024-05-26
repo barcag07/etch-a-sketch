244ea823cff053df2901d0b7b55d5e588f24acae
Andy Garcia <barcag07@icloud.com>
2024-05-26 18:32:44 -0500

    Add another div to apply flexbox properties
    A grid div was created so that it can be properly centered.

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,19 @@
 const gridContainer = document.querySelector("div");
-gridContainer.setAttribute("style", "display: flex; flex-wrap: wrap; width: 400px; height: 400px; outline: 3px solid red;");
+
+const grid = document.createElement("div");
+grid.setAttribute("style", "display: flex; flex-wrap: wrap; width: 400px; height: 400px; outline: 3px solid red;");
 
 // const test = document.createElement("p");
 // gridContainer.appendChild(test);
 
-//Create the 16 x 16 (256) grids that go into the grid container
+//Create the 16 x 16 (256) grid items that go into the grid
 
 for (let i = 1; i <= 256; i++) {
      const gridItem = document.createElement("div");
      gridItem.classList.add("grid-item","item-" + i);
      // gridItem.textContent = i;
      gridItem.setAttribute ("style", "height: 25px; width: 25px; outline: 1px solid blue;");
-     gridContainer.appendChild(gridItem);
+     grid.appendChild(gridItem);
 }
+
+gridContainer.appendChild(grid);
